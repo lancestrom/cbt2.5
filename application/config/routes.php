1d7c767d6dbe3ed9dfa32c870d3c819ac5975b03
lancestrom <dikakewren@gmail.com>
2025-10-11 23:24:47 +0700

    SISWA UPDATE 1

--- application/config/routes.php
+++ application/config/routes.php
@@ -3,6 +3,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 
 
-$route['default_controller'] = 'Login';
+$route['default_controller'] = 'Siswa_login';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
